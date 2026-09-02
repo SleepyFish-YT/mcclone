d16a63b33e50c3356f2eca1a1725dba78613af86
sleepy <sleepygoober69@gmail.com>
2026-09-02 22:12:42 +0200

    changed to be in minecraft.cpp, since its better this way!

--- src/me/sleepyfish/client/Minecraft.cpp
+++ src/me/sleepyfish/client/Minecraft.cpp
@@ -82,3 +82,235 @@ uint16_t Minecraft::getLimitFramerate() const {
     const uint16_t framerate_limit = 240;
     return this->isGamePaused() ? framerate_limit / 2 : framerate_limit;
 }
+
+void Minecraft::handleKeypress(int key, int scancode, int action, int mods) {
+    this->mcProfiler.startSection("keyboard");
+    {
+        GameSettings &settings = *this->gameSettings;
+
+        if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+            KeyBinding::setKeyBindState(key, true);
+            KeyBinding::onTick(key);
+        }
+
+        if (action == GLFW_RELEASE) {
+            KeyBinding::setKeyBindState(key, false);
+        }
+
+        if (action == GLFW_PRESS) {
+            if (key == settings.keyBindScreenshot.getKeyCode()) {
+                // ScreenshotHelper::saveScreenshot(...)
+            }
+
+            if (key == settings.keyBindPerspective.getKeyCode()) {
+                ++settings.thirdPersonView;
+
+                if (settings.thirdPersonView > 2) {
+                    settings.thirdPersonView = 0;
+                }
+
+                if (settings.thirdPersonView == 0) {
+                    // this->entityRenderer.loadEntityShader(this->getRenderViewEntity());
+                } else if (settings.thirdPersonView == 1) {
+                    // this->entityRenderer.loadEntityShader(nullptr);
+                }
+
+                settings.saveSettings();
+            }
+
+            // if (this->currentScreen == nullptr) {
+            {
+                // ESC - pause menu
+                if (key == GLFW_KEY_ESCAPE) {
+                    // this->displayInGameMenu();
+                }
+
+                // F3 + key shortcuts
+                if (mods & GLFW_MOD_CONTROL) {
+                    if (key == GLFW_KEY_F) {
+                        if (mods & GLFW_MOD_SHIFT) {
+                            settings.renderDistanceChunks--;
+                        } else {
+                            settings.renderDistanceChunks++;
+                        }
+
+                        settings.renderDistanceChunks = std::clamp(settings.renderDistanceChunks, 2, 32);
+                        settings.saveSettings();
+                    }
+
+                    // F3+A - reload renderers
+                    if (key == GLFW_KEY_A) {
+                        // this->renderGlobal.loadRenderers();
+                    }
+
+                    // F3+T - refresh resources
+                    if (key == GLFW_KEY_T) {
+                        // this->refreshResources();
+                    }
+
+                    if (key == GLFW_KEY_R) {
+                        // this->refreshResources();
+                    }
+
+                    if (key == GLFW_KEY_G) {
+                        settings.advancedItemTooltips = !settings.advancedItemTooltips;
+                        settings.saveSettings();
+                    }
+
+                    if (key == GLFW_KEY_B) {
+                        // this->renderManager.setDebugBoundingBox(!this->renderManager.isDebugBoundingBox());
+                    }
+
+                    if (key == GLFW_KEY_P) {
+                        settings.pauseOnLostFocus = !settings.pauseOnLostFocus;
+                        settings.saveSettings();
+                    }
+
+                    // F3+H - clear chat
+                    if (key == GLFW_KEY_H) {
+                        // this->ingameGUI.getChatGUI().clearChatMessages();
+                    }
+                }
+
+                // F1 - hide HUD
+                if (key == settings.keyBindHideGui.getKeyCode()) {
+                    // settings.hideGUI = !settings.hideGUI;
+                }
+
+                if (key == settings.keyBindToggleDebugOverlay.getKeyCode()) {
+                    settings.showDebugInfo = !settings.showDebugInfo;
+                }
+            }
+
+            for (int i = 0; i < 9; ++i) {
+                KeyBinding* keybinding = settings.keyBindHotbar[i];
+                if (keybinding != nullptr) {
+                    // if (this->thePlayer.isSpectator()) {
+                    //     this->ingameGUI.getSpectatorGui().func_175260_a(l);
+                    // } else {
+                    //     this->thePlayer.inventory.currentItem = l;
+                    // }
+                }
+            }
+
+            // Inventory
+            if (key == settings.keyBindInventory.getKeyCode()) {
+                // this->displayGuiScreen(GuiInventory(this->thePlayer));
+            }
+
+            // Drop item
+            if (key == settings.keyBindDrop.getKeyCode()) {
+                // if (!this->thePlayer.isSpectator()) {
+                //     this->thePlayer.dropOneItem((mods & GLFW_MOD_CONTROL) != 0);
+                // }
+            }
+
+            // Chat
+            if (key == settings.keyBindChat.getKeyCode()) {
+                // this->displayGuiScreen(GuiChat());
+            }
+
+            // Command
+            if (key == settings.keyBindCommand.getKeyCode()) {
+                // this->displayGuiScreen(GuiChat("/"));
+            }
+
+            // if (this->thePlayer.isUsingItem()) {
+            //     if (!settings.keyBindUseItem.isKeyDown()) {
+            //         this->playerController.onStoppedUsingItem(this->thePlayer);
+            //     }
+            // } else {
+            //     if (key = settings.keyBindAttack.getKeyCode()) {
+            //         this->leftClickMouse();
+            //     }
+            //     if (key = settings.keyBindUseItem.getKeyCode()) {
+            //         this->rightClickMouse();
+            //     }
+            //     if (key = settings.keyBindPickBlock.getKeyCode()) {
+            //         this->middleClickMouse();
+            //     }
+            // }
+        }
+    }
+    this->mcProfiler.endSection();
+}
+
+void Minecraft::handleMouseButton(int button, int action, int mods) {
+    this->mcProfiler.startSection("mouse");
+    {
+        int keyCode = 1000 + button;
+
+        KeyBinding::setKeyBindState(keyCode, action == GLFW_PRESS);
+
+        if (action == GLFW_PRESS) {
+            KeyBinding::onTick(keyCode);
+
+            if (keyCode == this->gameSettings->keyBindPickItem.getKeyCode()) {
+                // if (this->thePlayer.isSpectator()) {
+                //     this->ingameGUI.getSpectatorGui().func_175261_b();
+                // } else {
+                //     this->displayGuiScreen(GuiInventory(this->thePlayer));
+                // }
+                Logger::log("Middle click mouse");
+            }
+
+            if (keyCode == this->gameSettings->keyBindAttack.getKeyCode()) {
+                // this->leftClickMouse();
+                // Logger::log("Left click mouse");
+            }
+
+            if (keyCode == this->gameSettings->keyBindUseItem.getKeyCode()) {
+                // this->rightClickMouse();
+                // Logger::log("Right click mouse");
+            }
+
+            if (keyCode == this->gameSettings->keyBindMouseForward.getKeyCode()) {
+                // Logger::log("Forward click mouse");
+            }
+
+            if (keyCode == this->gameSettings->keyBindMouseBack.getKeyCode()) {
+                // Logger::log("Back click mouse");
+            }
+        }
+
+        if (action == GLFW_RELEASE) {
+            // if player is using item and key released, stop using
+            // if (!this->gameSettings.keyBindUseItem.isKeyDown()) {
+            //     this->playerController.onStoppedUsingItem(this->thePlayer);
+            // }
+        }
+    }
+    this->mcProfiler.endSection();
+}
+
+void Minecraft::handleMouseScroll(double xOffset, double yOffset) {
+    if (yOffset != 0) {
+        int delta = yOffset > 0 ? 1 : -1;
+
+        // if (this->minecraft->thePlayer.isSpectator()) {
+        //     if (this->minecraft->ingameGUI.getSpectatorGui().func_175262_a()) { // func_175262_a = this.SpectatorMenu != null
+        //         this->minecraft->ingameGUI.getSpectatorGui().func_175259_b(-delta);
+        //     } else {
+        //         float speed = std::clamp(thePlayer.capabilities.getFlySpeed() + delta * 0.005f, 0.0f, 0.2f);
+        //         thePlayer.capabilities.setFlySpeed(speed);
+        //     }
+        // } else {
+        // hotbar scroll
+        //     this->minecraft->thePlayer.inventory.changeCurrentItem(delta);
+        // }
+    }
+
+    if (xOffset != 0) {
+        // touchpad horizontal scroll - treat same as vertical
+        int delta = xOffset > 0 ? 1 : -1;
+        // this->minecraft->thePlayer.inventory.changeCurrentItem(delta);
+    }
+}
+
+void Minecraft::handleMouseMove(double x, double y) {
+    // if (!this->minecraft->inGameHasFocus) return;
+    // this->minecraft->entityRenderer.updateCameraAndRender(...)
+}
+
+void Minecraft::onFullscreenChange(bool fullscreen) {
+}
