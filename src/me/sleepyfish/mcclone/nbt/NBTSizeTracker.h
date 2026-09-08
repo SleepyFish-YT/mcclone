88d285453d9bb75b272246f7662516230115efed
sleepy <sleepygoober69@gmail.com>
2026-09-08 23:12:42 +0200

    looks better this way.

--- src/me/sleepyfish/mcclone/nbt/NBTSizeTracker.h
+++ src/me/sleepyfish/mcclone/nbt/NBTSizeTracker.h
@@ -32,10 +32,12 @@ public:
         this->read_var += bits / 8ll;
 
         if (this->read_var > this->max) {
-            throw std::runtime_error(std::format(
-                    "Tried to read NBT tag that was too big; tried to allocate: {} bytes where max allowed: {}",
-                    this->read_var, this->max
-            ));
+            throw std::runtime_error(
+                    std::format(
+                        "Tried to read NBT tag that was too big; tried to allocate: {} bytes where max allowed: {}",
+                        this->read_var, this->max
+                    )
+            );
         }
     }
 
